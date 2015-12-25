7e8b008825bf108cf5226acde96279f82f7f5618
Milan Pässler <me@petabyteboy.de>
2015-12-25 13:43:28 +0100

    make setup script install and use aur script

--- .setup.sh
+++ .setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #this works for arch and parabola
+#this also installs the "aur" script to install aur packages with little overhead
 sudo pacman -S imagemagick i3lock
-bash <(curl aur.sh) -si faba-icon-theme-git faba-mono-icons-git moka-icon-theme-git compton i3-gaps-next-git gtk-theme-arc-git
+sudo mv .aur.sh /usr/bin/aur
+sudo chown root:root /usr/bin/aur
+sudo chmod 755 /usr/bin/aur
+aur -si faba-icon-theme-git faba-mono-icons-git moka-icon-theme-git compton i3-gaps-next-git gtk-theme-arc-git
