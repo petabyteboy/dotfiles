89c9fe3eace01038ccf1cbffe937cf9ebab1a020
Milan Pässler <me@petabyteboy.de>
2015-12-25 13:38:08 +0100

    add basic setup script

---
+++ .setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#this works for arch and parabola
+sudo pacman -S imagemagick i3lock
+bash <(curl aur.sh) -si faba-icon-theme-git faba-mono-icons-git moka-icon-theme-git compton i3-gaps-next-git gtk-theme-arc-git
