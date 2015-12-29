5343864aecde6f1693b1982e0d4c0922852661f3
Milan Pässler <me@petabyteboy.de>
2015-12-29 14:59:31 +0100

    change lockscreen blur values to something nicer

--- .lock.sh
+++ .lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 import -silent -window root /tmp/screen.jpg
-convert /tmp/screen.jpg -scale 3% -scale 3333% -scale 33% -blur 0,20 -scale 303% /tmp/screen.png
+convert /tmp/screen.jpg -scale 5% -scale 2000% -scale 40% -blur 0,15 -scale 250% /tmp/screen.png
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png /tmp/screen.jpg
