c4e973b527fa3b2b761d6264db7636348ba44dcc
Milan Pässler <me@petabyteboy.de>
2015-12-29 02:51:46 +0100

    change blur effect for lock screen

--- .lock.sh
+++ .lock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-import -silent -window root /tmp/screen.png
-convert /tmp/screen.png -scale 5% -scale 2000% /tmp/screen.png
+import -silent -window root /tmp/screen.jpg
+convert /tmp/screen.jpg -scale 40% -blur 0x8 -scale 250% /tmp/screen.png
+#convert /tmp/screen.jpg -scale 5% -scale 2000% /tmp/screen.jpg
 i3lock -i /tmp/screen.png
-rm /tmp/screen.png
+rm /tmp/screen.png /tmp/screen.jpg
