18eb177de114393d7329a99a53db2c9a20951253
Milan Pässler <me@petabyteboy.de>
2015-12-29 13:47:40 +0100

    optimize lock screen blur to be fast and safe but not pixelized to do that the image is first strongly pixelized and then (to be fast) again scaled down slightly, then blurred strongly, and then scaled up again

--- .lock.sh
+++ .lock.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 import -silent -window root /tmp/screen.jpg
-convert /tmp/screen.jpg -scale 40% -blur 0x8 -scale 250% /tmp/screen.png
-#convert /tmp/screen.jpg -scale 5% -scale 2000% /tmp/screen.jpg
+convert /tmp/screen.jpg -scale 3% -scale 3333% -scale 33% -blur 0,20 -scale 303% /tmp/screen.png
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png /tmp/screen.jpg
