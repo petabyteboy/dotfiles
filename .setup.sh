e06fe362dc02e1d5a4191619b944817d4640e453
Milan Pässler <me@petabyteboy.de>
2016-01-25 16:50:33 +0100

    fix compton and better setup

--- .setup.sh
+++ .setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #this works for arch and parabola
 #this also installs the "aur" script to install aur packages with little overhead
-sudo pacman -S imagemagick i3lock rofi
-sudo mv .aur.sh /usr/bin/aur
+sudo cp .aur.sh /usr/bin/aur
 sudo chown root:root /usr/bin/aur
 sudo chmod 755 /usr/bin/aur
-aur -si faba-icon-theme-git faba-mono-icons-git moka-icon-theme-git compton i3-gaps-next-git gtk-theme-arc-git
+PATH=$PATH:/usr/bin/core_perl aur faba-icon-theme-git faba-mono-icons-git moka-icon-theme-git compton i3-gaps-next-git gtk-theme-arc-git
+sudo pacman -S imagemagick i3lock rofi rxvt-unicode feh xorg-xrdb
