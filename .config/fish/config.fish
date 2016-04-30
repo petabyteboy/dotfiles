2224e5bfb0f37981845dcbe7d199c98302dbb927
Milan Pässler <me@petabyteboy.de>
2016-04-30 10:17:41 +0200

    make ssh agent work with fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,29 @@
-# start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-        exec startx -- -keeptty
-    end
-end
-
 set EDITOR vim
 export EDITOR
+
+function agent
+	ssh-add -l; or ssh-add
+end
+
+function mosh
+	agent
+	/usr/bin/mosh $argv
+end
+
+function mosch
+	mosh --ssh 'ssh -p 62954' $argv
+end
+
+function ssh
+	agent
+	/usr/bin/ssh $argv
+end
+
+# start X at login
+if status --is-login
+	pkill ssh-agent
+	ssh-agent -c | sed 's/setenv/set -x/' | source
+	if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+		exec startx -- -keeptty >~/.xorg.log ^&1
+	end
+end
---
+++ .rofi.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rofi -show run -lines 10 -eh 2 -width 100 -padding 100 -color-window argb:f0000000,argb:0,argb:0 -color-normal argb:0000000,#ffffff,argb:0000000,argb:0000000,#567eff -font DejaVu\ Sans\ 13
